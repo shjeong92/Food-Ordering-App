e51bf180c3b5fd860c8c226770a26a2c88fb1223
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-04-30 16:59:20 +0900

    add a form on each items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./MealItem.module.css";
+import MealItemForm from './MealItemForm';
+
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
@@ -9,7 +11,7 @@ const MealItem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div><MealItemForm/></div>
     </li>
   );
 };
