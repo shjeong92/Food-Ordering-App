50e9af90d1b1e59bce6e4b5cfbfe8eb32c6152a1
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-05-01 18:14:00 +0900

    last commit

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css'
 import {CartContext} from '../../store/CartProvider'
 const HeaderCartButton = (props) => {
+  const [btnBump, setBtnBump] = useState(false);
+  
   const cartCtx = useContext(CartContext);
-
-  const numberOfCardItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  
+  const numberOfCardItems = items.reduce((curNumber, item) => {
     return curNumber +item.amount
   }, 0);
 
+  const btnStyles = `${styles.button} ${btnBump ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if(items.length===0){
+      return;
+    }
+    setBtnBump(true);
+    console.log("on");
+
+    const timer = setTimeout(()=>{setBtnBump(false)},300);
+    return () => {
+      console.log("off");
+      clearTimeout(timer);
+    }
+
+  }, [items])
   return (
-    <button onClick={props.handleCart} className={styles.button}>
+    <button onClick={props.handleCart} className={btnStyles}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
